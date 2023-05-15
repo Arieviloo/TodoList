9eb3c8023593e9f4046e6fe1aeef14f48a588913
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-05-15 12:00:06 -0300

    Init

---
+++ TodoList/List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Jadiê on 15/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    
+    }
+
+
+}
+
